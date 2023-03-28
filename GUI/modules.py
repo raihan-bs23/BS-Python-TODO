e7b9a7f08c94a72b021bd915c5c185496b2dac6d
wiz727 <Abu.Raihan@brainstation-23.com>
2023-03-29 01:50:33 +0600

    todo gui app with add, edit feature

--- GUI/modules.py
+++ GUI/modules.py
@@ -1,17 +1,72 @@
 import csv
+import datetime
 
 filename = "todo_list.csv"
 fields = ["timestamp", "todo", "status"]
 
 
 def get_todo_list():
-    list1 = []
-    print("************* TO DO List ***********\n")
     with open(filename, 'r') as csvData:
         reader = csv.DictReader(csvData)
-        rows = list(reader)
-        for i, item in enumerate(rows):
-            list1.append(item)
-            return list1
-        #     print(f"{i + 1}- {item['todo']}")
+        data = list(reader)
+        list1 = []
+        for i, item in enumerate(data):
+            list1.append(item['todo'])
+    return list1
 
+
+# def get_todo_list():
+#     with open(filename, 'r') as csvData:
+#         reader = csv.DictReader(csvData)
+#         data = list(reader)
+#         list1 = []
+#         for item in data:
+#             list1.append({"todo": item['todo'], "status": item['status']})
+#     return list1
+
+
+def write_todo(todo):
+    try:
+        with open(filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            rows = list(reader)
+    except FileNotFoundError:
+        rows = []
+
+    uInput = todo
+    # Add user input to list
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    uInput = uInput.title()
+    rows.append({"timestamp": timestamp, "todo": uInput, "status": "Undone"})
+
+    # Write list to CSV file
+    with open(filename, 'w', newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(rows)
+
+
+def edit_todo(todo, new_todo):
+    try:
+        with open(filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            rows = list(reader)
+    except FileNotFoundError:
+        rows = []
+
+    # print(">>>", rows)
+    # Find the todo item with the given ID
+    for row in rows:
+        if row['todo'] == todo:
+            # Update the todo text and timestamp
+            row["todo"] = new_todo.title()
+            print("-----", row['todo'])
+            row["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Write the modified rows back to the CSV file
+    with open(filename, 'w', newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(rows)
+
+# edit_todo('Bowing 727', 'test')
--- GUI/todo_GUI.py
+++ GUI/todo_GUI.py
@@ -6,62 +6,77 @@ First install PySimpleGUI package using -
 import PySimpleGUI as pygui
 import csv
 import datetime
-
+import random
+import modules
 
 filename = "todo_list.csv"
 fields = ["timestamp", "todo", "status"]
 
-pygui.theme('LightBlue')
-label = pygui.Text("Type a to-do : ", font=('Times New Roman', 12))
-input_box = pygui.InputText(tooltip="type your todo", key='todo')
-add_button = pygui.Button("ADD")
-edit_button = pygui.Button("EDIT")
-title = pygui.Text("*********** Your TO-DO **********")
-with open(filename, 'r') as csvData:
-    reader = csv.DictReader(csvData)
-    rows = list(reader)
-    for i, item in enumerate(rows):
-        print(f"{i + 1}- {item['todo']}")
-        list_box = pygui.Listbox(values=item['todo'], key='todos', enable_events=True, size=[45, 10])
+theme = ['Reddit', 'Dark', 'DarkBlue', 'DarkBrown', 'DarkGreen', 'DarkTeal9',
+         'DarkPurple', 'DarkRed', 'GreenMono', 'LightGreen', 'Material1', 'Material2',
+         'NeutralBlue', 'SandyBeach', 'SystemDefault', 'Tan', 'TealMono']
 
-window = pygui.Window("BS TO-DO App",
-                      layout= [[label], [input_box], [add_button],
-                               [title],
-                               [list_box, edit_button]],
-                      resizable=True, size=(400, 150),
+pygui.theme('TealMono')
+print(random.choice(theme))
+label = pygui.Text("Type a To-Do : ", font=('Times New Roman', 12))
+input_box = pygui.InputText(tooltip="type your todo", key='todo')
+add_button = pygui.Button('ADD',
+                          button_color=('white', '#4caf50'))
+edit_button = pygui.Button('EDIT',
+                           button_color=('white', '#2196f3'))
+complete_button = pygui.Button('Complete',
+                               button_color=('white', '#f44336'))
+
+title = pygui.Text("*********** Your TO-DO **********")
+list_title = pygui.Text("My Todo List", font=("Times new roman", 18))
+
+list_box = pygui.Listbox(values=modules.get_todo_list(), key='todos', enable_events=True, size=[70, 10])
+
+header = [
+    pygui.Text('To-do', pad=(0, 0), size=(15, 1), justification='c'),
+    pygui.Text('Status', pad=(0, 0), size=(30, 1), justification='c'),
+]
+
+heading = [header]
+
+# for row in heading:
+#     heading.append([
+#         pygui.Input(size=(15, 1), pad=(0, 0), key=(row, 0)),
+#         pygui.Input(size=(30,1), pad=(0, 0), key=(row, 1)),
+#
+#     ])
+window = pygui.Window("Brain Station 23 TO-DO Application",
+                      layout=[[label, input_box, add_button],
+                              [list_title],
+                              [heading, list_box],
+                              [edit_button, complete_button]],
+                      resizable=True, size=(700, 400),
                       font=('Times New Roman', 12),
                       element_justification='center',
-                      icon='BS_logo.ico')
+                      icon='BS_logo_1.ico')
 while True:
     event, key = window.read()
-    print(event)
-    print(key['todo'])
     match event:
         case 'ADD':
-            try:
-                with open(filename, 'r') as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    rows = list(reader)
-            except FileNotFoundError:
-                rows = []
-
-            uInput = key['todo']
-            # Add user input to list
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            uInput = uInput.title()
-            rows.append({"timestamp": timestamp, "todo": uInput, "status": "Undone"})
-
-            # Write list to CSV file
-            with open(filename, 'w', newline="") as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=fields)
-                writer.writeheader()
-                writer.writerows(rows)
+            modules.write_todo(key['todo'])
+            todos = modules.get_todo_list()
+            window['todos'].update(values=todos)
+        case 'EDIT':
+            todo_to_edit = key['todos'][0]
+            new_todo = key['todo']
+            print("----", todo_to_edit)
+            print(">>>>>>>", new_todo)
+            todos = modules.get_todo_list()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            modules.edit_todo(todo_to_edit, new_todo)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=key['todos'][0])
         case 'exit':
             print("Program exit, BYE !!")
             break
         case pygui.WIN_CLOSED:
             break
 
-
 window.close()
-
